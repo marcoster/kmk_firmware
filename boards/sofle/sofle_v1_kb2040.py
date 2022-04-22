e0d1b55072d48c234181674e36fc489010c90ecf
Marco Sterbik <madbadmax00@gmail.com>
2022-04-22 21:52:31 +0200

    sofle: add pcb, no encoder support, kee boar

---
+++ boards/sofle/sofle_v1_kb2040.py
@@ -0,0 +1,30 @@
+import board
+
+from kmk.kmk_keyboard import KMKKeyboard as _KMKKeyboard
+from kmk.matrix import DiodeOrientation
+from kmk.matrix import intify_coordinate as ic
+
+
+class KMKKeyboard(_KMKKeyboard):
+    col_pins = (
+        board.A1,
+        board.A0,
+        board.SCK,
+        board.MISO,
+        board.MOSI,
+        board.D10,
+    )
+    row_pins = (board.D5, board.D6, board.D7, board.D8, board.D9)
+    diode_orientation = DiodeOrientation.COL2ROW
+    data_pin = board.D1
+    rgb_pixel_pin = board.NEOPIXEL
+
+#    coord_mapping = []
+#    coord_mapping.extend(ic(0, x, 8) for x in range(6))
+#    coord_mapping.extend(ic(4, x, 8) for x in range(5, -1, -1))
+#    coord_mapping.extend(ic(1, x, 8) for x in range(6))
+#    coord_mapping.extend(ic(5, x, 8) for x in range(5, -1, -1))
+#    coord_mapping.extend(ic(2, x, 8) for x in range(8))
+#    coord_mapping.extend(ic(6, x, 8) for x in range(7, -1, -1))
+#    coord_mapping.extend(ic(3, x, 8) for x in range(3, 8))
+#    coord_mapping.extend(ic(7, x, 8) for x in range(7, 2, -1))
